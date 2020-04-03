8a18ac6166702660db50c08394ced956251fc003
user1-jpg <62719179+user1-jpg@users.noreply.github.com>
2020-04-03 11:17:29 +0200

    Lastet opp

---
+++ tobias_sitt_django_environment/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tobia\documents\tobias_sitt_django_environment\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
